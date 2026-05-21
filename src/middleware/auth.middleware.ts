dd356b61c8903ab20e856e5f5ce3afddaca21ad8
sakib404-hub <akibhossainsakib1040@gmail.com>
2026-05-21 23:41:09 +0600

    user validation with user exist and is_active

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -1,5 +1,8 @@
 import type { NextFunction, Request, Response } from "express";
 import { sendResponse } from "../utility/sendResponse";
+import jwt, { type JwtHeader, type JwtPayload } from 'jsonwebtoken'
+import config from "../config/config";
+import { pool } from "../database";
 
 
 const auth = () => {
@@ -11,8 +14,23 @@ const auth = () => {
 
         const accessToken = req.headers.authorization;
 
+
+
         if(!accessToken){
             return sendResponse(res, 401, false, 'Unauthorized access');
+        }
+
+        const decoded = jwt.verify(accessToken as string, config.jwtSecret as string) as JwtPayload;
+
+
+        const userExist = await pool.query(`SELECT * FROM users WHERE email=$1`, [decoded.email]);
+
+        const user = userExist.rows[0];
+
+        if(!user){
+            return sendResponse(res, 404, false, 'User not found!');
+        }else if(user.is_active){
+            return sendResponse(res, 404, false, 'Forbidden Access!');
         }
 
         next();
